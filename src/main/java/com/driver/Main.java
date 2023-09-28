590a4538fe501d062558b15c946983adda53adc4
BiteDragon <himanshusrivastav156@gmail.com>
2023-09-28 23:54:58 +0530

    making product public

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,25 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args){
+
+        Product p=new Product();
+
+    }
+    public static class Product{
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y+z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+
+
+    }
+
 }
